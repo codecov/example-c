85234b462dc7949df7fbc60935626f3fe80a4a8d
Sophia Guo <sophiag@ca.ibm.com>
2018-11-22 13:52:11 -0500

    reduce covrage testing
    Signed-off-by: Sophia Guo <sophiag@ca.ibm.com>

--- hello.c
+++ hello.c
@@ -6,8 +6,11 @@ int main()
 {
     if (t)
         printf("on this line\n");
-    else
+    else {
         printf("but not here\n");
+        printf("reduce covrage\n");
+        printf("reduce covrage\n");
+    }
 
     return 0;
 }
